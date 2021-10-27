e9ea7f17c13f0c6b39c5667ca799921877aa4a40
Jenni M <jennifermoulder17@gmail.com>
2021-10-27 12:50:32 +0100

    Update LinReg.py

--- LinReg.py
+++ LinReg.py
@@ -1,9 +1,7 @@
 import sklearn
 from sklearn.datasets import fetch_california_housing
-#import linear_regresion
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import pandas 
 
 dataset = fetch_california_housing()
 X = dataset.data
@@ -15,5 +13,4 @@ sklearn_regressor = LinearRegression().fit(X_train, Y_train)
 sklearn_train_accuracy = sklearn_regressor.score(X_train, Y_train)
 sklearn_test_accuracy = sklearn_regressor.score(X_test, Y_test)
 
-pandas.DataFrame([[sklearn_train_accuracy], [sklearn_test_accuracy]], ["Training Accuracy", "Test Accuracy"], ["Sklearn\'s Implementation"])
 print(sklearn_train_accuracy, sklearn_test_accuracy)
